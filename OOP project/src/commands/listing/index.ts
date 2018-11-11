029ed0d4b535afff008848ea2d1e6c09e1de0b9e
Martin Bechev <martinbechev@gmail.com>
2018-11-12 00:09:04 +0200

    rename shopping cart command file to checkout command

--- OOP project/src/commands/listing/index.ts
+++ OOP project/src/commands/listing/index.ts
@@ -1,4 +1,4 @@
 export * from './list-available-pets-command';
 export * from './list-available-products-command';
 export * from './list-pet-category-by-price-command';
-export * from './shopping-cart';
+export * from './checkout-command';
